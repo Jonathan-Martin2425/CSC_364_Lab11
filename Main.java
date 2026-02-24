a7a29417598ce4fc4529c10b6262dfd4f4e3d020
BLopez04 <bernardo41504@gmail.com>
2026-02-23 23:03:31 -0800

    - Initiate in Main

--- Main.java
+++ Main.java
@@ -1,18 +1,26 @@
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
 
-        for (int i = 0; i < 5; i++){
+        String broker = "tcp://test.mosquitto.org:1883";
+
+        OutsourcerPublisher pub = new OutsourcerPublisher(broker);
+        Outsourcer.getInstance().setPublisher(pub);
+
+        Thread outsourcerSub = new Thread(new OutsourcerSubscriber(broker));
+        outsourcerSub.start();
+
+        System.out.println("Outsourcer running");
+
+        for (int i = 0; i < 5; i++) {
             Thread prod = new Thread(new Producer());
             prod.start();
         }
-
-        for (int i = 0; i < 1; i++){
+        for (int i = 0; i < 1; i++) {
             Thread worker = new Thread(new LocalWorker());
-
             worker.start();
         }
 
-
+        System.out.println("Local system running: Producers + LocalWorker + Outsourcer");
     }
 }
